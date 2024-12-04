be0f07f2d43715f6bc4d551d24dbe0054f7f5d97
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2024-12-05 00:02:04 +0600

    Added error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,16 @@
-
+import { Link } from "react-router-dom";
+import errorImg from "../assets/404.gif"
 
 const Error = () => {
     return (
-        <div>
-            <h2>This is error page</h2>
+      <div className="max-w-7xl mx-auto">
+        <img src={errorImg} className="mx-auto" alt="" />
+        <div className="flex items-center justify-center">
+          <button className="bg-red-700  px-6 py-2 rounded-md text-white font-semibold text-lg">
+            <Link to="/">Back To Home</Link>
+          </button>
         </div>
+      </div>
     );
 };
 
